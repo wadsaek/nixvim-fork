e2fca0c8fb69ac78569a2dd5d6c7cb6982b93d74
wadsaek <wadsaek@gmail.com>
2024-09-27 10:03:17 +0300

    plugins/lsp_signature: added more tests

--- tests/test-sources/plugins/by-name/lsp_signature/default.nix
+++ tests/test-sources/plugins/by-name/lsp_signature/default.nix
@@ -2,4 +2,32 @@
   empty = {
     plugins.lsp_signature.enable = true;
   };
+
+  example = {
+    plugins.lsp_signature = {
+      enable = true;
+
+      settings = {
+        debug = true;
+        log_path = /home/wadsaek/.config/lsp_signature.log;
+        verbose = true;
+        bind  = true;
+        doc_lines = 5;
+
+        max_height = 10;
+        max_width = 45;
+        wrap = false;
+        floating_window = true;
+        floating_window_above_cur_line = false;
+        floating_window_off_x = "function() return 1 end";
+        fix_pos = true;
+        hint_inline = "function() return 'inline' end";
+        handler_opts.border = "shadow";
+        extra_trigger_chars = ["$"];
+
+        shadow_blend = 1;
+        select_signature_key = "<C-c>";
+      };
+    };
+  };
 }
